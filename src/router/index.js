14a68385e29980db15289710843a6c63c11eaf2b
brian <riceb53@gmail.com>
2020-12-15 21:56:41 -0500

    add static show page

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Login from "../views/Login.vue";
 import Logout from "../views/Logout.vue";
 import RecipesIndex from "../views/RecipesIndex.vue";
 import RecipesNew from "../views/RecipesNew.vue";
+import RecipesShow from "../views/RecipesShow.vue";
 
 Vue.use(VueRouter)
 
@@ -20,6 +21,11 @@ const routes = [
     path: "/logout",
     name: "logout",
     component: Logout 
+  },
+  { 
+    path: "/recipes/:id",
+    name: "recipes-show",
+    component: RecipesShow
   },
   { 
     path: "/recipes/new",
---
+++ src/views/RecipesShow.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="recipes-show">
+    <h1>{{ message }}</h1>
+    <h1>title: {{ recipe.title }}</h1>
+    <h1>ingredients: {{ recipe.ingredients }}</h1>
+    <h1>directions: {{ recipe.directions }}</h1>
+    <h1>prep_time: {{ recipe.prep_time }}</h1>
+    <h1>image_url: {{ recipe.image_url }}</h1>
+    <h1>chef: {{ recipe.user.email }}</h1>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from 'axios'
+export default {
+  data: function() {
+    return {
+      message: "Welcome to the show",
+      recipe: {}
+    };
+  },
+  created: function() {
+    this.recipesShow();
+  },
+  methods: {
+    recipesShow: function() {
+      console.log('in recipes show');
+      axios.get('/api/recipes/41').then(response => {
+        console.log(response.data); 
+        this.recipe = response.data;
+      })
+    }
+  }
+};
+</script>
